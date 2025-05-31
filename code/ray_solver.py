a0114ed515e1f83b79fc0e0082145a254eed7e24
Braisa <oterolemabrais@gmail.com>
2025-05-31 17:46:32 +0200

    Ray solving script

---
+++ code/ray_solver.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+# Wormhole parameters
+
+rho = 1
+a = 1
+M = 1
+
+# Metric radial function
+x = lambda l : 2*(np.abs(l) - a)/(np.pi*M)
+r = lambda l : rho + np.heaviside(np.abs(l)/a, 1) * M * (x(l)*np.arctan(x(l)) - .5*np.log(1+x(l)**2))
+dr_dl = lambda l : np.heaviside(np.abs(l)/a, 1) * 2/np.pi * np.sign(l) * np.arctan(x(l))
+
+def get_ray_origin(l0, theta0, phi0, theta_cs, phi_cs):
+
+    """
+
+    Computes parting position for a ray received by a camera standing at the point (l0, theta0, phi0) at t = 0 that is looking at the point (theta_cs, phi_cs).
+
+    """
+
+    # Incoming light ray momenta
+
+    p_l = lambda l, theta : -1 * np.cos(phi_cs) * np.sin(theta_cs)
+    p_theta = lambda l, theta : r(l) * np.cos(theta_cs)
+    p_phi = lambda l, theta : r(l) * np.sin(theta) * (-1 * np.sin(phi_cs) * np.sin(theta_cs))
+
+    # Conserved quantities
+
+    b = p_phi(l0, theta0)
+    B = p_theta(l0, theta0)**2 + b**2 * np.sin(theta0)**-2
+
+    # Ray differential equations
+    # coords = (l, theta, phi, p_l, p_theta)
+
+    diff = lambda t, coords : (coords[3],
+                               coords[4] * r(coords[0])**-2,
+                               b * r(coords[0])**-2 * np.sin(coords[1])**-2,
+                               B**2 * r(coords[0])**-3 * dr_dl(coords[0]),
+                               b**2 * r(coords[0])**-2 * np.cos(coords[1]) * np.sin(coords[1])**-3
+                               )
+        
+    # Initial coordinates
+    
+    initial_coords = (l0, theta0, phi0, p_l(l0, theta0), p_theta(l0, theta0))
+
+    # Runge-Kutta solving
+
+    return solve_ivp(fun = diff, t_span = (0, -1e5), y0 = initial_coords, method = "RK45")
