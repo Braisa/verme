5c95c59f2749f4733d253c2c2170d2bf54543e99
Braisa <oterolemabrais@gmail.com>
2025-05-31 22:12:17 +0200

    Changed sky building logic

--- code/sky_builder.py
+++ code/sky_builder.py
@@ -2,52 +2,59 @@ import numpy as np
 from PIL import Image
 from ray_solver import get_ray_origin
 
-# Result name
+# File parameters
 
-save_name = "test"
+save_name = "fig7b"
+output_width = 500
+output_height = 500
 
 # Camera position
 
-l_cam = 0
+l_cam = 6.75
 theta_cam = np.pi/2
 phi_cam = 0
 
 # Open celestial spheres' images
 
-upper_sphere_path = "assets/orange_sphere.png"
-lower_sphere_path = "assets/blue_sphere.png"
+upper_sphere_path = "assets/InterstellarWormhole_Fig6b-1750x875.jpg"
+lower_sphere_path = "assets/InterstellarWormhole_Fig6a-1750x875.jpg"
 
 upper_sphere_image = Image.open(upper_sphere_path)
 lower_sphere_image = Image.open(lower_sphere_path)
 
-upper_sphere_map = upper_sphere_image.load()
-lower_sphere_map = lower_sphere_image.load()
+upper_sphere_map = np.asarray(upper_sphere_image)
+lower_sphere_map = np.asarray(lower_sphere_image)
 
 # Get pixel size and step
 
-pixels_high, pixels_wide = upper_sphere_image.size
-
-theta_step = np.pi / pixels_high
-phi_step = 2*np.pi / pixels_wide
+theta_step = np.pi / output_height
+phi_step = 2*np.pi / output_width
 
 # Get pixel in corresponding celestial sphere for each pixel in the camera sky
 
-camera_sky = Image.new(mode = "RGB", size = (pixels_high, pixels_wide))
-camera_sky_map = camera_sky.load()
+camera_sky_map = np.zeros((output_height, output_width, 3), dtype = np.uint8)
 
-for n in range(pixels_high):
+for n in range(output_height):
     theta_cs = n * theta_step
-    for m in range(pixels_wide):
-        phi_cs = m * phi_step
-        
-        sol = get_ray_origin(l_cam, theta_cam, phi_cam, theta_cs, phi_cs)
-        l, theta, phi, _, _ = sol.y[:,-1]
-        sphere_n = np.round(theta/theta_step)
-        sphere_m = np.round(phi/phi_step)
+    for m in range(output_width):
+        if True:
+            phi_cs = m * phi_step
+            
+            print(n, m)
 
-        if l > 0:
-            camera_sky_map[n, m] = upper_sphere_map[sphere_n, sphere_m]
-        elif l < 0:
-            camera_sky_map[n, m] = lower_sphere_map[sphere_n, sphere_m]
+            sol = get_ray_origin(l_cam, theta_cam, phi_cam, theta_cs, phi_cs)
+            l, theta, phi, _, _ = sol.y[:,-1]
+            theta = np.abs(theta) % np.pi
+            phi = np.abs(phi) % 2*np.pi
 
-camera_sky.save(f"results/{save_name}.png")
+            if l > 0:
+                sphere_n = upper_sphere_image.size[1] * int(np.floor(theta/np.pi))
+                sphere_m = upper_sphere_image.size[0] * int(np.floor(phi/2/np.pi))
+                camera_sky_map[n, m] = upper_sphere_map[sphere_n, sphere_m]
+            elif l < 0:
+                sphere_n = lower_sphere_image.size[1] * int(np.floor(theta/np.pi))
+                sphere_m = lower_sphere_image.size[0] * int(np.floor(phi/2/np.pi))
+                camera_sky_map[n, m] = lower_sphere_map[sphere_n, sphere_m]
+
+camera_sky_image = Image.fromarray(camera_sky_map)
+camera_sky_image.save(f"results/{save_name}.jpg")
